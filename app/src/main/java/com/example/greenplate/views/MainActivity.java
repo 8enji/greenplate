f6aae25b951966a426c8d2958fd4a304384aa570
8enji <b.charest04@gmail.com>
2024-02-20 10:42:08 -0500

    setup mvvm file structure

--- app/src/main/java/com/example/greenplate/views/MainActivity.java
+++ app/src/main/java/com/example/greenplate/views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.greenplate;
+package com.example.greenplate.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.greenplate.R;
 
 public class MainActivity extends AppCompatActivity {
 
