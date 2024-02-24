c594d1e4ef939b81135bcaca69464e6ab9b42ded
cescoda6 <cescoda01@gmail.com>
2024-02-24 16:27:47 -0500

    This is the recipe screen

--- app/src/main/java/com/example/greenplate/views/RecipeScreen.java
+++ app/src/main/java/com/example/greenplate/views/RecipeScreen.java
@@ -1,15 +1,2 @@
-import android.os.Bundle;
-import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
-public class RecipeScreen extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe);
-
-        // Inflate the header layout
-        TextView headerTextView = findViewById(R.id.header);
-        headerTextView.setText("Recipe Screen");
-    }
-
+package com.example.greenplate.views;public class RecipeScreen {
 }
