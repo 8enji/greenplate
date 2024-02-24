909f203e42abafa29dbb2d2cc91ce9b52057d5d4
MirzaZuhayr <rafijzuhayr@gmail.com>
2024-02-23 20:45:38 -0500

    initial nav bar created and functional, waiting on other screens

--- app/src/main/java/com/example/greenplate/views/ShoppingList.java
+++ app/src/main/java/com/example/greenplate/views/ShoppingList.java
@@ -1,16 +1,24 @@
 package com.example.greenplate.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.greenplate.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class ShoppingList extends AppCompatActivity {
+    private BottomNavigationView bottom_navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        bottom_navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
     }
 }
