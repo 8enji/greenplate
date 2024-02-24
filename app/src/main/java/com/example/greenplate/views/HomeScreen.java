331c97303f04642d6dfc30a166be0f2de215215d
MirzaZuhayr <rafijzuhayr@gmail.com>
2024-02-24 14:08:48 -0500

    added functionality for navbar screen switching

--- app/src/main/java/com/example/greenplate/views/HomeScreen.java
+++ app/src/main/java/com/example/greenplate/views/HomeScreen.java
@@ -1,16 +1,34 @@
 package com.example.greenplate.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.greenplate.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeScreen extends AppCompatActivity {
-
+    private BottomNavigationView bottom_navigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        bottom_navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottom_navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.item_1) {
+                    Intent intent = new Intent(HomeScreen.this, ShoppingList.class);
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
     }
+
+
 }
