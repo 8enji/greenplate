be1a54779540e0b3081ec1cc0735d50dfc9a39ed
Connor Sempf <connortsempf@gmail.com>
2024-02-23 20:52:36 -0500

    Splash Screen implementation with transfer to Home Screen

---
+++ app/src/main/java/com/example/greenplate/views/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.greenplate.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.greenplate.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, HomeScreen.class);
+                startActivity(intent);
+                finish();
+            }
+
+
+        }, 3000);
+
+    }
+}
