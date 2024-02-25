9a0ed5b0c1d0c401a99951c34ef0d03241c01c94
Connor Sempf <connortsempf@gmail.com>
2024-02-25 15:11:25 -0500

    Updated some SplashScreen code for readability and maintainability

--- app/src/main/java/com/example/greenplate/views/SplashScreen.java
+++ app/src/main/java/com/example/greenplate/views/SplashScreen.java
@@ -9,24 +9,23 @@ import com.example.greenplate.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    // Splash Screen Implementation Variables
     Handler handler = new Handler();
+    Intent intent = new Intent(SplashScreen.this, UserLogin.class);
+    int milliDelay = 3000;
 
+    // Splash Screen Implementation Function
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, UserLogin.class);
                 startActivity(intent);
                 finish();
             }
-
-
-        }, 3000);
+        }, milliDelay);
 
     }
 }
