ca52e3dea13d158b3a720981be3e0872d43987f9
MirzaZuhayr <rafijzuhayr@gmail.com>
2024-02-25 17:07:17 -0500

    added functionality for navbar on shoppinglist screen

--- app/src/main/java/com/example/greenplate/views/ShoppingList.java
+++ app/src/main/java/com/example/greenplate/views/ShoppingList.java
@@ -20,5 +20,24 @@ public class ShoppingList extends AppCompatActivity {
         setContentView(R.layout.activity_shopping_list);
 
         bottom_navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottom_navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.item_1) {
+                    Intent intent = new Intent(ShoppingList.this, ShoppingList.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.item_2) {
+                    Intent intent = new Intent(ShoppingList.this, RecipeScreen.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.item_2) {
+                    Intent intent = new Intent(ShoppingList.this, IngredientScreen.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(ShoppingList.this, InputMeal.class);
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
     }
 }
