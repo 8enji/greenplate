5b84afa551db1997142b2a4f530ff3f138c8cdf2
enovu <sushanth26malipati@gmail.com>
2024-03-06 13:06:40 -0500

    made personal info screen

---
+++ app/src/main/java/com/example/greenplate/views/PersonalInfoScreen.java
@@ -0,0 +1,16 @@
+package com.example.greenplate.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.greenplate.R;
+
+public class PersonalInfoScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info_screen);
+    }
+}
