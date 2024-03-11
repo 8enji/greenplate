117d9d524ea817252c8ed9c0830958c9eaf6abef
Mirza Zuhayr <106599466+MirzaZuhayr@users.noreply.github.com>
2024-03-11 01:27:31 -0400

    Revert "Modified some of the input text - please undo"

--- app/src/main/java/com/example/greenplate/viewmodels/InputMealViewModel.java
+++ app/src/main/java/com/example/greenplate/viewmodels/InputMealViewModel.java
@@ -28,34 +28,34 @@ public class InputMealViewModel extends ViewModel {
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
         String email = currentUser.getEmail();
-        userRef = db.collection("Users").document(email);
+        userRef = db.collection("users").document(email);
     }
 
     public void createMeal(String mealName, String calories, AuthCallback callback) {
         if (mealName.isEmpty() || calories.isEmpty()) {
             //null checking
-            callback.onFailure("Meal Name and Calories are required.");
+            callback.onFailure("Meal Name and Calories are required");
             return;
         }
 
         if (calories.contains(" ")) {
             //calories will not parse to an int
-            callback.onFailure("Calories cannot contain whitespace.");
+            callback.onFailure("Calories cannot contain whitespace");
             return;
         }
         if (calories.contains("-")) {
             //calories cannot be negative
-            callback.onFailure("Calories cannot be negative.");
+            callback.onFailure("Calories cannot be negative");
             return;
         }
         try {
             // Attempt to parse the calories string to an integer
             int caloriesInt = Integer.parseInt(calories);
             Map<String, Object> meal = new HashMap<>();
-            meal.put("Name: ", mealName);
-            meal.put("Calories: ", caloriesInt);
+            meal.put("name", mealName);
+            meal.put("calories", caloriesInt);
 
-            userRef.collection("Meals").document(mealName)
+            userRef.collection("meals").document(mealName)
                     .set(meal)
                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
@@ -74,7 +74,7 @@ public class InputMealViewModel extends ViewModel {
 
         } catch (NumberFormatException e) {
             // Parse Fails
-            callback.onFailure("Calories must be a valid number.");
+            callback.onFailure("Calories must be a valid number");
             return;
         }
     }
