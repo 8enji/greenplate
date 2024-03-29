4dc23d15039229659347a4fe80a8e460ef79aec9
enovu <sushanth26malipati@gmail.com>
2024-03-29 18:58:17 -0400

    made ingredient form screen

--- app/src/main/java/com/example/greenplate/views/IngredientsFormScreen.java
+++ app/src/main/java/com/example/greenplate/views/IngredientsFormScreen.java
@@ -3,7 +3,6 @@ package com.example.greenplate.views;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,25 +23,30 @@ import com.example.greenplate.viewmodels.RecipeScreenViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class IngredientScreen extends AppCompatActivity {
+public class IngredientsFormScreen extends AppCompatActivity {
     private IngredientViewModel viewModel;
     private BottomNavigationView bottomNavigation;
 
-    private Button addIngredientButton;
-
-    private IngredientViewModel ingredientViewModel;
-
+    private EditText editTextIngredientName, editTextQuantity, editTextCalories;
+    private Button buttonSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ingredients_screen);
+        setContentView(R.layout.activity_ingredients_form_screen);
         viewModel = new ViewModelProvider(this).get(IngredientViewModel.class);
 
-        addIngredientButton = findViewById(R.id.ingredientButton);
+        editTextIngredientName = findViewById(R.id.editTextIngredientName);
+        editTextQuantity = findViewById(R.id.editTextQuantity);
+        editTextCalories = findViewById(R.id.editTextCaloriesPerServing);
+        buttonSave = findViewById(R.id.buttonSave);
 
-        addIngredientButton.setOnClickListener(v -> {
-            Intent intent = new Intent(IngredientScreen.this, IngredientsFormScreen.class);
-            startActivity(intent);
+
+
+        buttonSave.setOnClickListener(v -> {
+            String ingredientName = editTextIngredientName.getText().toString();
+            String quantity = editTextQuantity.getText().toString();
+            String calories = editTextCalories.getText().toString();
+            createIngredient(ingredientName, quantity, calories);
         });
 
         bottomNavigation = (BottomNavigationView) findViewById(R.id.bottomNavigation);
@@ -50,19 +54,19 @@ public class IngredientScreen extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getItemId() == R.id.shoppingnav) {
-                    Intent intent = new Intent(IngredientScreen.this, ShoppingList.class);
+                    Intent intent = new Intent(IngredientsFormScreen.this, ShoppingList.class);
                     startActivity(intent);
                 } else if (item.getItemId() == R.id.recipenav) {
-                    Intent intent = new Intent(IngredientScreen.this, RecipeScreen.class);
+                    Intent intent = new Intent(IngredientsFormScreen.this, RecipeScreen.class);
                     startActivity(intent);
                 } else if (item.getItemId() == R.id.ingredientnav) {
-                    Intent intent = new Intent(IngredientScreen.this, IngredientScreen.class);
+                    Intent intent = new Intent(IngredientsFormScreen.this, IngredientScreen.class);
                     startActivity(intent);
                 } else if (item.getItemId() == R.id.inputmealnav) {
-                    Intent intent = new Intent(IngredientScreen.this, InputMeal.class);
+                    Intent intent = new Intent(IngredientsFormScreen.this, InputMeal.class);
                     startActivity(intent);
                 } else if (item.getItemId() == R.id.personalinfonav) {
-                    Intent intent = new Intent(IngredientScreen.this, PersonalInfoScreen.class);
+                    Intent intent = new Intent(IngredientsFormScreen.this, PersonalInfoScreen.class);
                     startActivity(intent);
                 }
                 return false;
@@ -70,5 +74,20 @@ public class IngredientScreen extends AppCompatActivity {
         });
     }
 
+    private void createIngredient(String ingredientName, String quantity, String calories) {
+        viewModel.createIngredient(ingredientName, quantity, calories, new IngredientViewModel.AuthCallback() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(IngredientsFormScreen.this,
+                        "Ingredient added", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            @Override
+            public void onFailure(String error) {
+                Toast.makeText(IngredientsFormScreen.this,
+                        "Ingredient add failed: " + error, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 }
