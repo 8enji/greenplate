a622f3e4902646324b820207446689e91144c26b
MirzaZuhayr <rafijzuhayr@gmail.com>
2024-04-18 18:08:08 -0400

    made new xml for adapter list and fixing UI

--- app/src/main/java/com/example/greenplate/views/ShoppingAdapter.java
+++ app/src/main/java/com/example/greenplate/views/ShoppingAdapter.java
@@ -12,24 +12,24 @@ import java.util.ArrayList;
 import com.example.greenplate.model.Ingredient;
 import com.example.greenplate.viewmodels.ShoppingListViewModel;
 
-public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.IngredientViewHolder> {
+public class ShoppingAdapter extends RecyclerView.Adapter<ShoppingAdapter.ShoppingViewHolder> {
     private ArrayList<Ingredient> ingredients;
     private ShoppingListViewModel viewModel;
 
-    public IngredientAdapter(ArrayList<Ingredient> ingredients, ShoppingListViewModel viewModel) {
+    public ShoppingAdapter(ArrayList<Ingredient> ingredients, ShoppingListViewModel viewModel) {
         this.ingredients = ingredients;
         this.viewModel = viewModel;
     }
 
     @Override
-    public IngredientViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ShoppingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_ingredient, parent, false);
-        return new IngredientViewHolder(view);
+        return new ShoppingViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(IngredientViewHolder holder, int position) {
+    public void onBindViewHolder(ShoppingViewHolder holder, int position) {
         Ingredient ingredient = ingredients.get(position);
         holder.ingredientNameTextView.setText(String.format("%5s%5s%5s",
                 ingredient.getName(), ingredient.getQuantity(), ingredient.getUnits()));
@@ -37,7 +37,7 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.In
         holder.buttonIncrease.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewModel.increaseIngredientQuantity(ingredient, new IngredientViewModel.IngredientUpdateCallback() {
+                viewModel.increaseIngredientQuantity(ingredient, new ShoppingListViewModel().IngredientUpdateCallback() {
                     @Override
                     public void onIngredientUpdated(boolean success, String error) {
                         if (success) {
@@ -51,7 +51,7 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.In
         holder.buttonDecrease.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewModel.decreaseIngredientQuantity(ingredient, new IngredientViewModel.IngredientUpdateCallback() {
+                viewModel.decreaseIngredientQuantity(ingredient, new ShoppingListViewModel.IngredientUpdateCallback() {
                     @Override
                     public void onIngredientUpdated(boolean success, String error) {
                         if (success) {
@@ -69,12 +69,12 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.In
         return ingredients.size();
     }
 
-    static class IngredientViewHolder extends RecyclerView.ViewHolder {
+    static class ShoppingViewHolder extends RecyclerView.ViewHolder {
         TextView ingredientNameTextView;
         Button buttonIncrease;
         Button buttonDecrease;
 
-        public IngredientViewHolder(View itemView) {
+        public ShoppingViewHolder(View itemView) {
             super(itemView);
             ingredientNameTextView = itemView.findViewById(R.id.ingredientNameTextView);
             buttonIncrease = itemView.findViewById(R.id.buttonIncrease);
